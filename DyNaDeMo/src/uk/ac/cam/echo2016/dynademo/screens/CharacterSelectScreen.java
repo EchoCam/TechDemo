21faa25a6d8bcd5ec4b0a3a24fceafd56668af6c
Tom Maenan Read-Cutting <readcuttingt@gmail.com>
2016-02-25 18:34:37 +0000

    Add code for dynamic buttons in character select
    Adding new buttons to the character select screen, such that the number
    of them and their functionality can vary is almost there. From here
    testing that should be pretty straight forward, also need to add ability
    to flush them out of the screen as well.

--- DyNaDeMo/src/uk/ac/cam/echo2016/dynademo/screens/CharacterSelectScreen.java
+++ DyNaDeMo/src/uk/ac/cam/echo2016/dynademo/screens/CharacterSelectScreen.java
@@ -8,8 +8,13 @@ import com.jme3.app.Application;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.builder.PanelBuilder;
+import de.lessvoid.nifty.controls.button.builder.ButtonBuilder;
+import de.lessvoid.nifty.elements.Element;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
+import java.util.ArrayList;
+import java.util.List;
 import uk.ac.cam.echo2016.dynademo.MainApplication;
 
 /**
@@ -21,6 +26,7 @@ public class CharacterSelectScreen extends AbstractAppState implements ScreenCon
     private Nifty nifty;
     private Screen screen;
     private MainApplication app;
+    private List<String> currentChars;
 
     public CharacterSelectScreen() {
         super();
@@ -39,6 +45,40 @@ public class CharacterSelectScreen extends AbstractAppState implements ScreenCon
     @Override
     public void onStartScreen() {
         app.getFlyByCamera().setDragToRotate(true);
+        currentChars = new ArrayList<>();
+
+        //This piece of code should be loading in routes from dynamic narrative
+        {
+            currentChars.add("Brandon");
+            currentChars.add("Dooby");
+        }
+
+        Element bottomPanel = nifty.getCurrentScreen().findElementByName("panel_bottom");
+        for (final String character : currentChars) {
+            //Add character to screen...
+            PanelBuilder p = new PanelBuilder("character_" + character) {
+                {
+                    childLayoutCenter();
+                    valignCenter();
+                    backgroundColor("#0008");
+                    height("25%");
+                    width("25%");
+
+                    // add control
+                    control(new ButtonBuilder("selectCharacter(" + character + ")", character) {
+                        {
+                            alignCenter();
+                            valignCenter();
+                            height("50%");
+                            width("50%");
+                            visibleToMouse(true);
+                        }
+                    });
+                }
+            };
+            bottomPanel.add(p.build(nifty, screen, bottomPanel));
+        }
+        nifty.getCurrentScreen();
     }
 
     @Override
