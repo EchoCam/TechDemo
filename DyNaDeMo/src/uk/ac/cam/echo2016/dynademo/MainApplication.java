e6baa6824ce35a3e16a3b01bcc712d97bad30410
James <jr650@cam.ac.uk>
2016-02-26 22:57:18 +0000

    Change lever to two valued logic

--- DyNaDeMo/src/uk/ac/cam/echo2016/dynademo/MainApplication.java
+++ DyNaDeMo/src/uk/ac/cam/echo2016/dynademo/MainApplication.java
@@ -432,8 +432,9 @@ public class MainApplication extends SimpleApplication implements DemoListener {
                 // TODO should check parent nodes for physics controls?
                 if (rbc == null)
                     throw new NullPointerException("No valid physics control found for object: " + spatial.getName());
-                spatial.rotate(0f, FastMath.PI/2, -FastMath.PI/2); // Three way rotation
-                
+                spatial.rotate(-FastMath.PI/4, 0f, 0f); // Three way rotation
+                spatial.rotate(0f, FastMath.PI, 0f);
+                spatial.rotate(FastMath.PI/4, 0f, 0f);
                 break;
             default:
                 System.out.println("Error: Event type: " + eInter.getType() + " not recognized");
