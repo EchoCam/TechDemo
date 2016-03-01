f586a2128ee66ce31bc9cf182c319155b282d050
Tim Ringland <ringlandtim@gmail.com>
2016-03-01 15:28:51 +0000

    more head stuff

--- DyNaDeMo/src/uk/ac/cam/echo2016/dynademo/HeadMoveTask.java
+++ DyNaDeMo/src/uk/ac/cam/echo2016/dynademo/HeadMoveTask.java
@@ -2,7 +2,8 @@ package uk.ac.cam.echo2016.dynademo;
 
 public class HeadMoveTask extends DemoTask {
     private HeadObject headObj;
-    private boolean recentMove = false;
+    private boolean prevLight = false;
+    private int movecount;
     private MainApplication app;
     
     public HeadMoveTask(String taskQueueId, float completionTime, HeadObject headOjb, MainApplication app) {
@@ -12,18 +13,10 @@ public class HeadMoveTask extends DemoTask {
     
     @Override
     public void onTimeStep(float timePassed){
-        if (app.getLightsOn()) {
-            recentMove = false;
-        } else {
-            if (!recentMove && app.getLightsOffCount() > 1) {
-                moveHead();
-                
-                
-                
-                recentMove = true;
-            }
+        if (app.getLightsOn() && prevLight == false && app.getLightsOffCount() > 10) {
+            moveHead();
         }
-        
+        prevLight = app.getLightsOn();
     }
     public void moveHead() {
         System.out.println("moved");
